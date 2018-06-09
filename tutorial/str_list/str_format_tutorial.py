3ef1650f7f44666c4890bce75244c9fb6cb6513d
yayo256 <>
2018-06-10 00:34:40 +0900

    str_format_tutorial, capitalize 修正

--- tutorial/str_list/str_format_tutorial.py
+++ tutorial/str_list/str_format_tutorial.py
@@ -2,4 +2,4 @@ str_list = ["text", "python", "discord", "connect", "clock"]
 x = max([len(s) for s in str_list])
 
 for s in str_list:
-    print("{: >{}}".format(s, x))
+    print("{: >{}}".format(s.capitalize(), x))
