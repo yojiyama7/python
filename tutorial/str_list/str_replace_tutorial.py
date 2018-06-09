9e129a5ac29fa7c67af13f47cadeca647e627704
yayo256 <>
2018-06-10 01:33:04 +0900

    str_replace_tutorial　修正

--- tutorial/str_list/str_replace_tutorial.py
+++ tutorial/str_list/str_replace_tutorial.py
@@ -1,6 +1,4 @@
 str_list = ["apple_pen", "pineapple_pen", "pen_pineapple/apple_pen"]
 
-for i, s in enumerate(str_list):
-    str_list[i] = s.replace("_", " ")
-
-print(str_list)
+for s in str_list:
+    print(s.replace("_", " "))
