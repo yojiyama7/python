7517cc861cb9a4b986c939b159da2764eea62315
yayo256 <>
2018-06-10 01:16:46 +0900

    str_remove_tutorial　別解追加

--- tutorial/str_list/str_remove_tutorial.py
+++ tutorial/str_list/str_remove_tutorial.py
@@ -4,3 +4,11 @@ while "" in str_list:
     str_list.remove("")
 
 print(str_list)
+
+# str_list = ["", "defg", "hijklmn", "", ""]
+#
+# empty_str_count = str_list.count("")
+# for i in range(empty_str_count):
+#     str_list.remove("")
+#
+# print(str_list)
