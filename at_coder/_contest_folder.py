42ebf5c8e04bb4f141c33a388163e685bc823c58
yayo256 <>
2018-07-19 07:36:10 +0900

    add and edit

---
+++ at_coder/_contest_folder.py
@@ -0,0 +1,17 @@
+import os.path
+
+# abs_path = os.path.dirname(os.path.abspath(__file__))
+# def rel_path(path):
+# 	return abs_path + "/" + path
+
+contest_num = input("abcの番号: ")
+contest_name = "abc_{}".format(contest_num)
+if contest_num == "":
+	print("入力してください")
+elif os.path.isdir(contest_name):
+	print("その番号はすでに存在します")
+else:
+	os.mkdir(contest_name)
+	for c in "abcdef":
+		with open("{}/{}_.py".format(contest_name, c), "w", encoding="utf-8") as f:
+			f.write("")
