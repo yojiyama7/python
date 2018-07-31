6005c8e864a3d4579946ec2b409c94818585eb3e
YAYOSPC\yayo256 <yojiyama7@gmail.com>
2018-07-31 12:15:57 +0900

    contest_abc a問題, b問題 to 103 all complate

--- at_coder/abc/abc_035/b_drone.py
+++ at_coder/abc/abc_035/b_drone.py
@@ -1,18 +1,9 @@
 s = input()
 t = int(input())
 
-# わからん
-
-x, y, unknown = 0, 0, 0
-for s_i in s:
-	if s_i == "L":
-		x -= 1
-	elif s_i == "R":
-		x += 1
-	elif s_i == "U":
-		y += 1
-	elif s_i == "D":
-		y -= 1
-	elif s_i == "?":
-		unknown += 1
-print(abs(x)+abs(y) + (unknown if t == 1 else -unknown))
+l, r, u, d, unknown = [s.count(c) for c in "LRUD?"]
+distance = abs(r-l) + abs(u-d)
+if t==1:
+	print(distance + unknown)
+else:
+	print(max(distance - unknown, (distance - unknown)%2))
