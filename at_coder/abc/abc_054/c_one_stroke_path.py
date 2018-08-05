e25193f79d193b4e5646a2928bda8ffd19712b01
YAYOSPC\yayo256 <yojiyama7@gmail.com>
2018-08-05 23:27:25 +0900

    abc_104 and some c probrems

---
+++ at_coder/abc/abc_054/c_one_stroke_path.py
@@ -0,0 +1,2 @@
+n, m = map(int, input().split(" "))
+ab = [tuple(map(int, input().split(" "))) for i in range(m)]
---
+++ at_coder/abc/abc_104/c_all_green.py
@@ -0,0 +1,3 @@
+d, g = map(int, input().split(" "))
+pc = [list(map(int, input().split(" "))) for _ in range(d)]
+
