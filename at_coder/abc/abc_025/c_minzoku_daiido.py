1910defb8771902fba9ef8e9c266dbc247105750
yayo256 <>
2018-08-27 00:16:11 +0900

    c

---
+++ at_coder/abc/abc_025/c_minzoku_daiido.py
@@ -0,0 +1,9 @@
+def range_combi(a, b):
+	return (min(min(a), min(b)), max(max(a), max(b)))
+
+def is_in_range(n, a)
+
+# n, d, k = map(int, input().split(" "))
+# lr = [tuple(map(int, input().split(" "))) for _ in range(d)]
+# st = [tuple(map(int, input().split(" "))) for _ in range(k)]
+
