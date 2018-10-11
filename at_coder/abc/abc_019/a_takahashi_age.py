51caea3c90a5a589f8e835bc3e6720bd990986cf
yayo256 <>
2018-10-11 19:40:11 +0900

    a

--- at_coder/abc/abc_019/a_takahashi_age.py
+++ at_coder/abc/abc_019/a_takahashi_age.py
@@ -1,5 +1,5 @@
-# abc = map(int, input().split(" "))
+abc = map(int, input().split(" "))
 
-# print(sorted(abc)[1])
+print(sorted(abc)[1])
 
-print(sorted(map(int,input().split()))[1])
+# print(sorted(map(int,input().split()))[1])
--- at_coder/abc/abc_053/a_abc_arc.py
+++ at_coder/abc/abc_053/a_abc_arc.py
@@ -1,3 +1,5 @@
 x = int(input())
 
 print("ABC" if x < 1200 else "ARC")
+
+# print("AARBCC"[input()<"1200"::2])
