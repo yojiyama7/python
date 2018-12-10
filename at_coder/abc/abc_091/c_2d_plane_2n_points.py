12a47f96d75f7d14a2aa4ac839c6376a6dfe386b
yayo256 <>
2018-12-10 15:57:07 +0900

    abc c problems

--- at_coder/abc/abc_091/c_2d_plane_2n_points.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-ab = [tuple(map(int, input().split(" "))) for i in range(n)]
-cd = [tuple(map(int, input().split(" "))) for i in range(n)]
--- at_coder/abc/abc_092/c_traveling_plan.py
+++ at_coder/abc/abc_092/c_traveling_plan.py
@@ -1,5 +1,16 @@
-n = int(input())
-a = list(map(int, input().split(" ")))
+from itertools import accumulate
 
-a = [0] + a + [0]
-a_walks = [abs(a[i+1]-a[i]) for i in range(n+1)]
+N = int(input())
+A = list(map(int, input().split(" ")))
+
+A = [0] + A + [0]
+total_cost = sum(abs(A[i+1]-A[i]) for i in range(N+1))
+
+one, two, three = [None] + A[:2]
+for i in range(N):
+	one, two, three = two, three, A[i+2]
+	first, second = two-one, three-two
+	cost_to_reduce = 0
+	if (first < 0) != (second < 0):
+		cost_to_reduce = min(abs(first), abs(second))*2
+	print(total_cost - cost_to_reduce)
