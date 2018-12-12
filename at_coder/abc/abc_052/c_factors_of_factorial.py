73dee50f870eeb4b0c48bc91af4195fc9d982e47
yayo256 <>
2018-12-12 16:03:45 +0900

    abc c problems

--- at_coder/abc/abc_052/c_factors_of_factorial.py
+++ at_coder/abc/abc_052/c_factors_of_factorial.py
@@ -1,17 +1,23 @@
-# よくわかっとらん
-n = int(input())
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-counts = [0]*(n+1)
-for i in range(2, n+1):
-    for prime in primes:
-        while i % prime == 0:
-            counts[prime] += 1
-            i //= prime
-    if i > 1:
-        counts[i] += 1
+# わがらんぞー
 
-print(counts)
-score = 1
-for count in counts:
-    score = (score * (count + 1)) % (10**9+7)
-print(score)
+################################
+#-------------------------------
+################################
+
+# よくわかっとらん
+# n = int(input())
+# primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+# counts = [0]*(n+1)
+# for i in range(2, n+1):
+#     for prime in primes:
+#         while i % prime == 0:
+#             counts[prime] += 1
+#             i //= prime
+#     if i > 1:
+#         counts[i] += 1
+
+# print(counts)
+# score = 1
+# for count in counts:
+#     score = (score * (count + 1)) % (10**9+7)
+# print(score)
