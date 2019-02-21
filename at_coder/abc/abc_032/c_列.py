541f080829d31302254c07f7c724b7cffd5dad7b
yayo256 <>
2019-02-21 14:36:46 +0900

    abc c problems 020, 025, 032

--- at_coder/abc/abc_032/c_列.py
+++ at_coder/abc/abc_032/c_列.py
@@ -1,8 +1,39 @@
-# https://beta.atcoder.jp/contests/abc032/submissions/3181449
+# 尺取り法は、軸を移動するたびに
+# それによる影響を(変数に)反映させることで、計算量をO(N)に抑えることができる
+
+N, K = map(int, input().split(" "))
+S = [int(input()) for _ in range(N)]
+
+def result():
+    if 0 in S:
+        return N
+        
+    l, r = 0, 0
+    max_len = 0
+    p = 1
+    for i in range(N):
+        # print(l, r, p, max_len)
+        # 右軸をpがK以下になる範囲で右へ移動
+        while r < N and p*S[r] <= K:
+            p *= S[r]
+            r += 1
+        # 最大なら更新
+        max_len = max(max_len, r-l)
+        # 左軸を1つ右へ移動
+        p //= S[l]
+        l += 1
+        # 追い越し防止
+        if l > r:
+            r = l
+            p = 1
+
+    return max_len
+
+print(result())
 
 ################################
-#-------------------------------
-################################
+
+# https://beta.atcoder.jp/contests/abc032/submissions/3181449
 
 # # WA TLE
 
