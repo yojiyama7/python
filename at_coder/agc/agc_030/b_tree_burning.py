5aa2b3498511def22ddcbc02ed4055dec243f2a2
yayo256 <>
2019-02-22 14:26:33 +0900

    agc 030 a

--- at_coder/agc/agc_030/b_tree_burning.py
+++ at_coder/agc/agc_030/b_tree_burning.py
@@ -1,3 +1,31 @@
+# # not submit
+
+# from itertools import chain, zip_longest
+
+# L, N = map(int, input().split(" "))
+# X = [int(input()) for _ in range(N)]
+
+# zip_chain = lambda a, b: list(chain(*zip_longest(a, b)))
+# def remove_all(a, x):
+#     while x in a:
+#         a.remove(x)
+#     return a
+# # print(X[::2], X[1::2])
+
+# l1 = zip_chain((L-x for x in X[::2]), X[1::2])
+# l2 = zip_chain(X[::-2], (L-x for x in X[-1::-2]))
+
+# l1, l2 = [remove_all(l_i, None) for l_i in [l1, l2]]
+
+# print(l1, l2)
+
+# a1 = l1[0] + 2*sum(l1[1:-1]) + l1[-1]
+# a2 = l2[0] + 2*sum(l2[1:-1]) + l1[-1]
+
+# print(max(a1, a2))
+
+################################
+
 # from bisect import bisect_left
 
 # L, N = map(int, input().split(" "))
